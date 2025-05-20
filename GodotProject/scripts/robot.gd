216ef6a9936c8237f6c1ed654b5295c8d0ac00c9
Daniele Materia <danielemateria14@gmail.com>
2025-05-20 23:06:58 +0200

    Debug labels X,Y

--- GodotProject/scripts/robot.gd
+++ GodotProject/scripts/robot.gd
@@ -3,6 +3,7 @@ extends Node3D
 var theRobot
 var left_motor : HingeJoint3D
 var right_motor : HingeJoint3D
+@onready var xy_text_edit = $"../CanvasGroup/XY"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +12,9 @@ func _ready():
 	DDS.subscribe("X")
 	DDS.subscribe("Y")
 	DDS.subscribe("Theta")
-
+	
+	print(theRobot.global_position.x, " ", -theRobot.global_position.z, " ", theRobot.global_rotation.y)
+	_edit_xy_text(theRobot.global_position.x, -theRobot.global_position.y)
 
 func _physics_process(delta):
 	pass
@@ -25,12 +28,18 @@ func _process(_delta):
 	var x = DDS.read("X")
 	var y = DDS.read("Y")
 	var theta = DDS.read("Theta")
+	
 
 	if (x != null)and(y != null)and(theta != null):
 		theRobot.global_position.x = x
 		theRobot.global_position.z = -y
 		theRobot.global_rotation.y = theta
 
+		_edit_xy_text(x,-y)
+
 
 func _integrate_forces(state):
 	pass
+
+func _edit_xy_text(x, y):
+	xy_text_edit.text = "(%.2f, %.2f)" % [x,y]
