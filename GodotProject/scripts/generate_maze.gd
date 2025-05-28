303a4cdfb860c1d810660f0e9950702ccf8a945c
Daniele Materia <danielemateria14@gmail.com>
2025-05-28 19:37:10 +0200

    Colliders funzionanti, bug fix vari e implementazione area di uscita dal labirinto

--- GodotProject/scripts/generate_maze.gd
+++ GodotProject/scripts/generate_maze.gd
@@ -53,16 +53,34 @@ func _dfs(pos: Vector2i):
 				_dfs(Vector2i(nx, ny))
 
 func build_floor():
-	var floor = MeshInstance3D.new()
-	floor.mesh = PlaneMesh.new()
-	floor.mesh.size = Vector2(maze_width * cell_size, maze_height * cell_size)
+	var floor_body = StaticBody3D.new()
+	floor_body.name = "MazeFloor"
+	floor_body.collision_layer = 1
+	floor_body.collision_mask = 1
+
+	var floor_mesh = MeshInstance3D.new()
+	var plane_mesh = PlaneMesh.new()
+	plane_mesh.size = Vector2(maze_width * cell_size, maze_height * cell_size)
+	floor_mesh.mesh = plane_mesh
 
 	var floor_material = StandardMaterial3D.new()
 	floor_material.albedo_color = Color(0.75, 0.85, 0.75) # verde salvia chiaro
+	floor_mesh.material_override = floor_material
 
-	floor.material_override = floor_material
-	floor.transform.origin = Vector3((maze_width * cell_size) / 2 - cell_size / 2, 0, (maze_height * cell_size) / 2 - cell_size / 2)
-	add_child(floor)
+	# Posizione centrata
+	var center_x = (maze_width * cell_size) / 2 - cell_size / 2
+	var center_z = (maze_height * cell_size) / 2 - cell_size / 2
+	floor_body.transform.origin = Vector3(center_x, 0, center_z)
+
+	var collider = CollisionShape3D.new()
+	var shape = BoxShape3D.new()
+	shape.size = Vector3(maze_width * cell_size, 0.1, maze_height * cell_size)
+	collider.shape = shape
+	collider.transform.origin = Vector3(0, -0.05, 0) # leggermente sotto per evitare intersezioni
+
+	floor_body.add_child(floor_mesh)
+	floor_body.add_child(collider)
+	add_child(floor_body)
 
 func build_maze():
 	var wall_size_h = Vector3(cell_size, wall_height, wall_thickness)
@@ -84,9 +102,13 @@ func build_maze():
 				_place_wall(Vector3(cx, wall_height / 2, cz + cell_size / 2), wall_size_h, wall_material)
 			if cell[3]: # West
 				_place_wall(Vector3(cx - cell_size / 2, wall_height / 2, cz), wall_size_v, wall_material)
+				
+	var goal_pos = Vector3(maze_width * cell_size, 0, (maze_height - 1) * cell_size)
+	create_goal_area(goal_pos)
 
 func _place_wall(position: Vector3, size: Vector3, material: Material):
 	var wall_body = StaticBody3D.new()
+	wall_body.add_to_group("walls")
 	wall_body.transform.origin = position
 	wall_body.collision_layer = 1
 	wall_body.collision_mask = 1
@@ -105,3 +127,50 @@ func _place_wall(position: Vector3, size: Vector3, material: Material):
 	wall_body.add_child(collider)
 
 	add_child(wall_body)
+
+func create_goal_area(position: Vector3):
+	# --- CREA UNA PIATTAFORMA SOLIDA ---
+	var platform_body = StaticBody3D.new()
+	platform_body.transform.origin = position + Vector3(0, 0.01, 0)  # leggero offset per evitare z-fighting
+
+	var platform_mesh = MeshInstance3D.new()
+	var mesh = BoxMesh.new()
+	mesh.size = Vector3(cell_size, 0.1, cell_size)
+	platform_mesh.mesh = mesh
+
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color(0.6, 0.85, 0.9)
+	mesh.material = material
+	platform_body.add_child(platform_mesh)
+
+	var collider = CollisionShape3D.new()
+	var shape = BoxShape3D.new()
+	shape.size = Vector3(cell_size, 0.1, cell_size)
+	collider.shape = shape
+	platform_body.add_child(collider)
+
+	add_child(platform_body)
+
+	# --- CREA L'AREA DI GOAL SOPRA LA PIATTAFORMA ---
+	var goal_area = Area3D.new()
+	goal_area.name = "GoalArea"
+	goal_area.monitoring = true
+	goal_area.collision_layer = 2
+	goal_area.collision_mask = 1  # il robot deve avere layer 1
+
+	var goal_collider = CollisionShape3D.new()
+	var goal_shape = BoxShape3D.new()
+	goal_shape.size = Vector3(1.0, 1.0, 1.0)
+	goal_collider.shape = goal_shape
+	goal_collider.transform.origin = Vector3(0, 0.5, 0)
+
+	goal_area.transform.origin = position + Vector3(0, 0.05, 0)
+	goal_area.add_child(goal_collider)
+
+	goal_area.connect("body_entered", Callable(self, "_on_goal_area_entered"))
+	add_child(goal_area)
+
+func _on_goal_area_entered(body):
+	if body.is_in_group("robot"):
+		print("Obiettivo raggiunto!")
+		DDS.publish("GoalReached", DDS.DDS_TYPE_INT, 1)
--- GodotProject/scripts/robot.gd
+++ GodotProject/scripts/robot.gd
@@ -1,35 +1,90 @@
 extends Node3D
 
-var theRobot
-var left_motor : HingeJoint3D
-var right_motor : HingeJoint3D
 @onready var xy_text_edit = $"../CanvasGroup/XY"
+@onready var theRobot = $"."
 
-# Called when the node enters the scene tree for the first time.
+var x := 0.0
+var y := 0.0
+var theta := 0.0
+var step_size := 0.2
+var angle_step := deg_to_rad(15)
+
+var collided := false
+
 func _ready():
-	#inizializzazione server
-	theRobot = $"."
+	theRobot.add_to_group("robot")
+	DDS.publish("GoalReached", DDS.DDS_TYPE_INT, 0)
+	DDS.subscribe("Action")
 	DDS.subscribe("X")
 	DDS.subscribe("Y")
 	DDS.subscribe("Theta")
 	
-	#print(theRobot.global_position.x, " ", -theRobot.global_position.z, " ", theRobot.global_rotation.y)
-	_edit_xy_text(theRobot.global_position.x, -theRobot.global_position.z)
+	var area = $Area3D
+	area.connect("body_entered", Callable(self, "_on_collision_with_wall"))
 
-func _physics_process(delta):
-	pass
+	_edit_xy_text(x, y)
+	
+func _on_collision_with_wall(body):
+	if body.is_in_group("walls"):
+		collided = true
+		print("Collision!")
+		
 
+func _physics_process(_delta):
+	var action = DDS.read("Action")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	if action != null:
+		var prev_x = x
+		var prev_y = y
+		var prev_theta = theta
+		print("Received action:", action)
+
+		match action:
+			0:
+				# Avanza
+				x += step_size * cos(theta)
+				y += step_size * sin(theta)
+			1:
+				# Ruota a destra
+				theta -= angle_step
+			2:
+				# Ruota a sinistra
+				theta += angle_step
+
+		# Aggiorna posizione
+		theRobot.global_position.x = x
+		theRobot.global_position.z = -y
+		theRobot.rotation.y = theta
+
+		if collided:
+			print("Collision! Movement reverted.")
+			# Ripristina posizione e segnala collisione
+			x = prev_x
+			y = prev_y
+			theta = prev_theta
+			theRobot.global_position.x = x
+			theRobot.global_position.z = -y
+			theRobot.rotation.y = theta
+			collided = false
+			DDS.publish("Collision", DDS.DDS_TYPE_INT, 1)
+		else:
+			DDS.publish("Collision", DDS.DDS_TYPE_INT, 0)
+
+	_edit_xy_text(x, y)
+
 func _process(_delta):
-	#print(theRobot.global_position.x, " ", -theRobot.global_position.z, " ", theRobot.global_rotation.y)
 	DDS.publish("tick", DDS.DDS_TYPE_FLOAT, _delta)
-
+	
 	var x = DDS.read("X")
 	var y = DDS.read("Y")
 	var theta = DDS.read("Theta")
 	
-	# solo per testare i collider
+	if (x != null)and(y != null)and(theta != null):
+		theRobot.global_position.x = x
+		theRobot.global_position.z = -y
+		theRobot.global_rotation.y = theta
+		
+	# solo debug
 	if Input.is_action_pressed("move_dx"):
 		theRobot.global_position.x += 0.05
 	elif Input.is_action_pressed("move_sx"):
@@ -38,18 +93,8 @@ func _process(_delta):
 		theRobot.global_position.z -= 0.05
 	elif Input.is_action_pressed("move_down"):
 		theRobot.global_position.z += 0.05
-	
-
-	if (x != null)and(y != null)and(theta != null):
-		theRobot.global_position.x = x
-		theRobot.global_position.z = -y
-		theRobot.global_rotation.y = theta
-
-	_edit_xy_text(theRobot.global_position.x, -theRobot.global_position.z)
-
-
-func _integrate_forces(state):
-	pass
+		
+	_edit_xy_text(theRobot.global_position.x,theRobot.global_position.z)
 
 func _edit_xy_text(x, y):
 	xy_text_edit.text = "(%.2f, %.2f)" % [x,y]
